45c00395d413145b5d223a8d1971a3fc24a0e175
Andrea Belfiore <anbelf7@gmail.com>
2022-08-23 19:48:48 +0100

    Update article-preview-component

--- pages/article-preview-component/components/Social.js
+++ pages/article-preview-component/components/Social.js
@@ -3,6 +3,7 @@ import avatar from '../images/avatar-michelle.jpg';
 import facebook from '../images/icon-facebook.svg';
 import pinterest from '../images/icon-pinterest.svg';
 import twitter from '../images/icon-twitter.svg';
+import Share from '../images/icon-share.svg';
 import { ShareIcon } from '@heroicons/react/outline';
 import { useState } from 'react';
 
@@ -19,38 +20,82 @@ const Social = () => {
           <Image src={avatar} height={50} width={50} className="rounded-full" />
           <div className="mr-auto ml-5 flex flex-col justify-between">
             <h3 className="text-sm font-bold">Michelle Appleton</h3>
-            <p className="text-desaturated-dark-blue text-xs tracking-wide">
+            <p className="text-xs tracking-wide text-desaturated-dark-blue">
               28 Jun 2020
             </p>
           </div>
           <span
             onClick={share}
-            className="bg-grayish-blue flex h-8 w-8 cursor-pointer items-center justify-center rounded-full p-1"
+            className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-light-grayish-blue p-1 text-very-dark-grayish-blue"
           >
-            <ShareIcon className="text-light-grayish-blue h-5" />
+            <ShareIcon className="h-5" />
           </span>
         </div>
       ) : (
-        <div className="bg-very-dark-grayish-blue text-grayish-blue flex h-16 items-center justify-between rounded-b-lg px-5">
-          <h3 className="uppercase tracking-widest">share</h3>
-          <div className="mr-auto ml-10 flex space-x-5">
-            <span>
-              <Image src={facebook} />
-            </span>
-            <span>
-              <Image src={pinterest} />
-            </span>
-            <span>
-              <Image src={twitter} />
+        <>
+          {/* Mobile: flex lg:hidden */}
+          <div className="-mx-5 -mb-5 flex h-16 items-center justify-between rounded-b-lg bg-very-dark-grayish-blue px-10 text-grayish-blue lg:hidden">
+            <h3 className="uppercase tracking-widest">share</h3>
+            <div className="mr-auto ml-10 flex space-x-5">
+              <span>
+                <Image src={facebook} alt="facebook" />
+              </span>
+              <span>
+                <Image src={pinterest} alt="pinterest" />
+              </span>
+              <span>
+                <Image src={twitter} alt="twitter" />
+              </span>
+            </div>
+            <span
+              onClick={share}
+              className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-grayish-blue p-1"
+            >
+              <ShareIcon className="h-5 text-light-grayish-blue" />
             </span>
           </div>
-          <span
-            onClick={share}
-            className="bg-grayish-blue flex h-8 w-8 cursor-pointer items-center justify-center rounded-full p-1"
-          >
-            <ShareIcon className="text-light-grayish-blue h-5" />
-          </span>
-        </div>
+
+          {/* Desktop: hidden lg:flex */}
+          <div className="relative hidden h-16 items-center justify-between px-5 lg:flex">
+            <Image
+              src={avatar}
+              height={50}
+              width={50}
+              alt="avatar"
+              className="rounded-full"
+            />
+            <div className="mr-auto ml-5 flex flex-col justify-between">
+              <h3 className="text-sm font-bold">Michelle Appleton</h3>
+              <p className="text-xs tracking-wide text-desaturated-dark-blue">
+                28 Jun 2020
+              </p>
+            </div>
+            <span
+              onClick={share}
+              className=" flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-light-grayish-blue p-1 text-very-dark-grayish-blue"
+            >
+              <ShareIcon className="h-5" />
+            </span>
+
+            <div className="absolute right-8 bottom-16 flex translate-x-1/2 items-center justify-center rounded-lg bg-very-dark-grayish-blue py-4 px-8 shadow-lg">
+              <div className="absolute -bottom-2 h-5 w-5 rotate-45 bg-very-dark-grayish-blue shadow-lg"></div>
+              <h3 className="uppercase tracking-widest text-grayish-blue">
+                share
+              </h3>
+              <div className="mr-auto ml-10 flex space-x-5">
+                <span>
+                  <Image src={facebook} alt="facebook" />
+                </span>
+                <span>
+                  <Image src={pinterest} alt="pinterest" />
+                </span>
+                <span>
+                  <Image src={twitter} alt="twitter" />
+                </span>
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
--- pages/article-preview-component/index.js
+++ pages/article-preview-component/index.js
@@ -1,33 +1,39 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import Social from './components/Social';
 import img from './images/drawers.jpg';
 
 const index = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-200">
-      <div className="mx-auto flex h-screen w-full flex-col items-center justify-center  bg-slate-500 px-10 sm:w-2/3 sm:flex-row">
-        <Image
-          src={img}
-          objectFit="contain"
-          className="w-full rounded-t-lg  sm:rounded-l-lg sm:rounded-tr-none"
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;700&display=swap"
+          rel="stylesheet"
         />
-
-        <div className="bg-light-grayish-blue flex flex-col rounded-b-lg sm:rounded-r-lg sm:rounded-bl-none">
-          <div className="space-y-5 p-5">
-            <h2 className="text-lg font-bold">
-              Shift the overall look and feel by adding these wonderful touches
-              to furniture in your home
-            </h2>
-            <p className="text-desaturated-dark-blue text-sm leading-6">
-              Ever been in a room and felt like something was missing? Perhaps
-              it felt slightly bare and uninviting. I’ve got some simple tips to
-              help you make any room feel complete.
-            </p>
+      </Head>
+      <div className="flex h-screen items-center justify-center bg-light-grayish-blue font-manrope">
+        <div className="m-3 flex h-2/3 flex-col items-center justify-center sm:w-[67%] lg:h-[20rem] lg:flex-row">
+          <div className="relative h-full w-full overflow-hidden rounded-t-lg lg:w-[45%] lg:rounded-l-lg lg:rounded-tr-none">
+            <Image src={img} layout="fill" objectFit="cover" alt="image" />
           </div>
-          <Social />
+          <div className="flex h-full flex-1 flex-col justify-center rounded-b-lg bg-white p-5  lg:rounded-r-lg lg:rounded-bl-none">
+            <div className="space-y-5 p-5">
+              <h2 className="text-lg font-bold">
+                Shift the overall look and feel by adding these wonderful
+                touches to furniture in your home
+              </h2>
+              <p className="text-sm leading-6 text-desaturated-dark-blue ">
+                Ever been in a room and felt like something was missing? Perhaps
+                it felt slightly bare and uninviting. I’ve got some simple tips
+                to help you make any room feel complete.
+              </p>
+            </div>
+            <Social />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
